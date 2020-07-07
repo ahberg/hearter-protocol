04b6adb03b56b1e954b61db7b07f2bd820723dd7
Anton <anton@happyorder.io>
2020-07-07 20:02:17 +0200

    pointsLeft fixed

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,96 +1,119 @@
 <template>
-    <table class="game_table">
-      <thead>
-        <tr>
-          <td>Spelare</td>
-          <td v-for="(player) in players" :key="player.index">
-            <input class="name" type="text" v-model="player.name" />
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(game,gameId) in gameInfo" :key="game.index" >
-          <td>{{ game.name }}</td>
-          <td v-for="(player,playerId) in players" :key="player.index">
-            <input  v-on:input="calcPoints(playerId,gameId),calcPointsleft(playerId,gameId)" type="text" v-model.number="points[playerId][gameId]" />
-          </td>
-          <td> {{   pointsLeft[gameId] }} </td>
-        </tr>
+  <table class="game_table">
+    <thead>
+      <tr>
+        <td>Spelare</td>
+        <td v-for="(player) in players" :key="player.index">
+          <input class="name" type="text" v-model="player.name" />
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(game,gameId) in gameInfo" :key="game.index">
+        <td>{{ game.name }}</td>
+        <td v-for="(player,playerId) in players" :key="player.index">
+          <input
+            v-on:input="calcPoints(playerId,gameId),calcPointsleft(playerId,gameId)"
+            type="text"
+            v-model.number="points[playerId][gameId]"
+          />
+        </td>
+        <td>{{ pointsLeft[gameId] }}</td>
+      </tr>
       <tr>
         <td>Poäng</td>
-        <td  v-for="(player,playerId) in players" :key="player.index">
-        <span class="points" >{{ players[playerId].total }}</span>
+        <td v-for="(player,playerId) in players" :key="player.index">
+          <span class="points">{{ players[playerId].total }}</span>
         </td>
       </tr>
     </tbody>
-    </table>
-
+  </table>
 </template>
 
 <script>
 const gameInfo = [
   {
-    name: 'H',
+    name: "H",
     points: 65
   },
   {
-    name: 'D',
+    name: "D",
     points: 40
   },
   {
-    name: '3M',
+    name: "3M",
     points: 45
   },
   {
-    name: 'SS',
+    name: "SS",
     points: 20
   },
   {
-    name: 'Totti',
+    name: "Totti",
     points: 155
   }
-]
+];
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
 
-  data () {
+  data() {
     return {
       gameInfo: gameInfo,
       numberOfPlayers: 4,
       players: [],
       points: [],
-      pointsLeft: [gameInfo[0].points,gameInfo[1].points,gameInfo[2].points,gameInfo[3].points,gameInfo[4].points]
-    }
+      pointsLeft: [
+        gameInfo[0].points,
+        gameInfo[1].points,
+        gameInfo[2].points,
+        gameInfo[3].points,
+        gameInfo[4].points
+      ]
+    };
   },
   methods: {
-    calcPoints: function (playerId, gameId) {
+    calcPoints: function(playerId, gameId) {
       // calc player points
-      let numOr0 = n => isNaN(n) ? 0 : n
-      this.players[playerId].total = this.points[playerId].reduce((a, b) =>
-        numOr0(a) + numOr0(b))
+      let numOr0 = n => (isNaN(parseInt(n)) ? 0 : n)
+      this.players[playerId].total = this.points[playerId].reduce((a, b) => { 
+      return numOr0(a) + numOr0(b) 
+      
+      });
       // calcPointsleft(gameId);
     },
-    calcPointsleft: function (playerId, gameId) {
-      let sum = this.gameInfo[gameId].points
-      this.pointsLeft[gameId]= sum - this.points[playerId][gameId]
-      
+    calcPointsleft: function(playerId, gameId) {
+      let sum = this.gameInfo[gameId]
+      let zeros = [];
+      sum = sum - this.points[playerId][gameId];
+      this.pointsLeft[gameId] = sum;
+      if (sum === 0) {
+        this.points.forEach((p,id) => {
+          if(!p[gameId]){
+            zeros.push(id);
+          }
+        });
+      console.log(zeros)
+      zeros.forEach((pid) => {
+        this.points[pid][gameId]  = Math.round(-50 / zeros.length);
+      });
+      }
     }
   },
-  mounted () {
+  mounted() {
     while (this.players.length < this.numberOfPlayers) {
-      this.players.push({ name: '', total: 0 })
-      this.points.push([null, null, null, null, null])
+      this.players.push({ name: "", total: 0 });
+      this.points.push([null, null, null, null, null]);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.name[type=text] {
+.name[type="text"] {
   font-weight: bold;
   text-align: center;
-  text-transform:uppercase;
+  text-transform: uppercase;
 }
 
 .points {
@@ -99,6 +122,6 @@ export default {
   margin-right: auto;
   font-weight: bold;
   text-align: center;
-  font-size: 20px
+  font-size: 20px;
 }
 </style>
