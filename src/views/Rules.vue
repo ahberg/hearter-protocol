a9a3974e65ddb8458fc625f6eaf073c636d22a71
Anton <anton@happyorder.io>
2020-07-23 15:49:19 +0200

    Added rules

---
+++ src/views/Rules.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="rules">
+    <h1>Hur man spelar poänghjärter</h1>
+   <p>Antal spelare: 3-7
+    <br/>Kortlek: Standardlek om 52 kort där ess är högst.
+    <br/>Given: Vem som helst kan börja som givare men efter varje parti flyttas given medsols.</p>
+    <p>Man delar sedan ut korten så långt det räcker men ingen spelare får ha fler kort än någon annan. Övriga kort läggs åt sidan, de får dock inte innefatta hjärterkort, damer, ruter 10 eller klöver 2. Dessa övriga kort visas för alla spelare. Spelaren till vänster om givaren börjar.</p>
+    <br/>
+    <p>Så här spelas varje parti: Man spelar 5 partier och varje parti spelas med stick och man måste följa färg. Har man inte den givna färgen får man saka ett kort. Det finns ingen trumf. Högsta kortet i den givna färgen vinner sticket.</p>
+    <p>Det här går spelet ut på: Man ska undvika att ta vissa kort/stick. Dessa är olika beroende på parti. De kort/stick man ska undvika ger poäng och det ska man undvika att få.</p>
+    <p>Vilka kort/stick ska undvikas?
+    <br/>
+    Parti 1: Alla kort i färgen hjärter.
+    Parti 2: Alla damer.
+    <br/>Parti 3: Klöver 2, ruter 10 samt spader dam.
+    <br/>Parti 4: Sista sticket.
+    <br/>Parti 5: Allt som man i parti 1-4 har undvikit.
+    <br/>
+    <br/>Poäng: De kort/stick man ska undvika ger <br/> poäng på följande vis:
+    <br/>Parti 1: Hjärter = 5 poäng per kort.
+    <br/>Parti 2: Damer = 10 poäng per dam.
+    <br/>Parti 3: Klöver 2 = 15 poäng, ruter 10 = 15 poäng, spader dam = 15 poäng.
+    <br/>Parti 4: Sista sticket = 20 poäng.
+    <br/>Parti 5: Poängen gäller på samma sätt som i parti 1-4 (hjärter dam = 10p spader dam = 15p)</p>
+    <p>Lyckas man att inte vinna något stick innehållande de man ska undvika får man -50 poäng. Är man flera som lyckats med detta delar man på -50 poäng. Är de exempelvis två som lyckats med detta får man vardera -25 poäng.</p>
+    <p>Vem vinner: Den som efter parti 5 har samlat ihop lägst antal poäng.</p>
+  </div>
+</template>
+
+
+<style scoped>
+ .rules {
+  max-width: 40em; 
+ }
+  h1 {
+    text-align: center;  
+  }
+</style>>
