ff183d0d97fa8478a95ac133aed7da5543151533
Anton <anton@dsv.su.se>
2025-07-18 13:51:13 +0200

    Add origin style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
-  <div>
+  <div >
     <div id="nav">
       <router-link to="/">Protokoll</router-link> |
       <router-link :to="{ name: 'Rules' }">Regler</router-link>
@@ -12,19 +10,3 @@ import HelloWorld from './components/HelloWorld.vue'
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
