59e27d3974f1fbf8709b03abe6f19aa9a502a30d
Anton <anton@dsv.su.se>
2025-07-28 16:17:24 +0200

    set timezone

--- public/save.php
+++ public/save.php
@@ -2,7 +2,8 @@
 	$json = file_get_contents('php://input');
 	$data = json_decode($json);
 	$st = ($post);
+	date_default_timezone_set('Europe/Stockholm');
 	$date = date(' Y-m-d H:i');
-	file_put_contents('log/game.log', $json . $date . "\n", FILE_APPEND);
+	file_put_contents('../log/game.log', $json . $date . "\n", FILE_APPEND);
 	echo 'yes';
 ?>
